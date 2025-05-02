c9b89f91f2c18b2bff4d9b9ea2f3609ebf7cd154
adamvir <virag.adam23@gmail.com>
2025-05-02 17:26:59 +0700

    added ch_count to main

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
-def get_book_text(file_path):
-    
-    with open(file_path, "r") as f:
-    
-        file_contents = f.read()
-
-    return file_contents
-
-def word_count(text):
-    word_count = 0
-    for word in text.split():
-        word_count += 1
-    
-    return word_count
-
-
+from stats import book_word_count, book_ch_count
 
 def main():
     file_path = "/Users/viragadam/desktop/bootdev/bookbot/books/frankenstein.txt"
     
-    num_words = word_count(get_book_text(file_path))
+    num_words = book_word_count(file_path)
+    ch_count =  book_ch_count(file_path)
 
     print(f"{num_words} words found in the document")
+    print(ch_count)
 
 main()
 
