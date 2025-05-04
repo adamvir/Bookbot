5145c1c948fa9183035063cf642217b060a8fae1
adamvir <virag.adam23@gmail.com>
2025-05-04 13:01:34 +0200

    update 1.0: only need the bookname to run the program

--- main.py
+++ main.py
@@ -2,21 +2,44 @@ import sys
 from stats import book_word_count, book_ch_count, list_ch_count
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python3 main.py <path_to_book>")
-        sys.exit(1)
+    file_path = input("📚Drop here the book name to count📚: ")
+    file_path = f"books/{file_path}.txt"
+    num_words = book_word_count(file_path)
 
-    else:
-        file_path = sys.argv[1]
-        num_words = book_word_count(file_path)
-        print("============ BOOKBOT ============")
-        print(f"Analyzing book found at {file_path}...")
-        print("----------- Word Count ----------")
-        print(f"Found {num_words} total words")
-        print("--------- Character Count -------")
-        list_ch_count(file_path)
-        print("============= END ===============")
-        sys.exit(0)
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            content = f.read()
+            print("✅ File loaded successfully.")
+    
+    except Exception as e:
+        print(f"❌ Error: {e}")
+
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")       
+    print("--------- Character Count -------")
+    list_ch_count(file_path)
+    print("============= END ===============")
+
+    
+    
+    # if len(sys.argv) < 2:
+    #     print("Usage: python3 main.py <path_to_book>")
+    #     sys.exit(1)
+
+    # else:
+    #     file_path = sys.argv[1]
+    #     num_words = book_word_count(file_path)
+    #     print("============ BOOKBOT ============")
+    #     print(f"Analyzing book found at {file_path}...")
+    #     print("----------- Word Count ----------")
+    #     print(f"Found {num_words} total words")
+    #     print("--------- Character Count -------")
+    #     list_ch_count(file_path)
+    #     print("============= END ===============")
+    #     sys.exit(0)
 main()
                    
         
